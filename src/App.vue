a061e39f58b012ca7ebbbca0f8c274a01651c3b3
MD Rana Prodhania <ranaprodhania@gmail.com>
2024-02-05 21:20:20 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Login from './views/auth/LoginView.vue';
+// import AccountView from './views/auth/AccountView.vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+// import WishlistView from './views/WishlistView.vue';
+// import ThanksView from './views/ThanksView.vue';
+
+// import CompareView from './views/CompareView.vue';
+// import CartView from './views/CartView.vue';
+// import CheckoutView from './views/CheckoutView.vue';
+
+
+
+</script>
+
+<template>
+  <Header/>
+  <Login />
+  <!-- <CartView /> -->
+  <!-- <CheckoutView /> -->
+  <!-- <CompareView /> -->
+  <!-- <AccountView /> -->
+  <!-- <WishlistView /> -->
+  <!-- <ThanksView /> -->
+  <Footer/>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
